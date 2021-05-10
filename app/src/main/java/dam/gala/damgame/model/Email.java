7ebf00382308b13a8e848193decf76314196ecdf
Juan Antonio <dam2ajuanantonioescribanodiaz@iesantoniogala.es>
2021-05-10 09:32:22 +0200

    Documentación del Email

--- app/src/main/java/dam/gala/damgame/model/Email.java
+++ app/src/main/java/dam/gala/damgame/model/Email.java
@@ -8,7 +8,6 @@ import dam.gala.damgame.utils.GameUtil;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
